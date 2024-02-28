a1ec29cc02d74b12193e0d4521dffee730d83a40
Roman <65170713+politerus@users.noreply.github.com>
2024-02-28 11:38:34 +0200

    5

--- src/main/java/ua/javarush/MyFirstTelegramBot.java
+++ src/main/java/ua/javarush/MyFirstTelegramBot.java
@@ -50,13 +50,51 @@ public class MyFirstTelegramBot extends TelegramLongPollingBot {
             if (update.getCallbackQuery().getData().equals("step_2_btn") && getGlories(chatId) == 20) {
                 addGlories(chatId,20);
                 SendMessage message =   createMessage(chatId, STEP_3_TEXT,
-                        Map.of("Злам робота пилососа", "step_2_btn"));
+                        Map.of("Злам робота пилососа", "step_3_btn"));
+                sendApiMethodAsync(message);
+            }
+            if (update.getCallbackQuery().getData().equals("step_3_btn") && getGlories(chatId) == 40) {
+                addGlories(chatId,30);
+                SendMessage message =   createMessage(chatId, STEP_4_TEXT,
+                        Map.of("Відправити робопилосос за їжею! +30 слави", "step_4_btn",
+                                "Проїхатися на робопилососі! +30 слави", "step_4_btn",
+                                "Тікати від робопилососа! +30 слави", "step_4_btn"));
+                sendApiMethodAsync(message);
+            }
+            if (update.getCallbackQuery().getData().equals("step_4_btn") && getGlories(chatId) == 70) {
+                addGlories(chatId,30);
+                SendMessage message =   createMessage(chatId, STEP_5_TEXT,
+                        Map.of("Одягнути та включити GoPro!", "step_5_btn"));
+                sendApiMethodAsync(message);
+            }
+            if (update.getCallbackQuery().getData().equals("step_5_btn") && getGlories(chatId) == 100) {
+                addGlories(chatId,40);
+                SendMessage message =   createMessage(chatId, STEP_6_TEXT,
+                        Map.of("Бігати дахами, знімати на GoPro! +40 слави", "step_6_btn",
+                                "З GoPro нападати на інших котів із засідки! +40 слави", "step_6_btn",
+                                "З GoPro нападати на собак із засідки! +40 слави", "step_6_btn"));
+                sendApiMethodAsync(message);
+            }
+            if (update.getCallbackQuery().getData().equals("step_6_btn") && getGlories(chatId) == 140) {
+                addGlories(chatId,40);
+                SendMessage message =   createMessage(chatId, STEP_7_TEXT,
+                        Map.of("Злам пароля", "step_7_btn"));
+                sendApiMethodAsync(message);
+            }
+            if (update.getCallbackQuery().getData().equals("step_7_btn") && getGlories(chatId) == 180) {
+                addGlories(chatId,50);
+                SendMessage message =   createMessage(chatId, STEP_8_TEXT,
+                        Map.of("Вийти на подвір'я", "step_8_btn"));
+                sendApiMethodAsync(message);
+            }
+            if (update.getCallbackQuery().getData().equals("step_8_btn") && getGlories(chatId) == 230) {
+               // addGlories(chatId,50);
+                SendMessage message =   createMessage(chatId, FINAL_TEXT);
                 sendApiMethodAsync(message);
             }
 
 
-
-        }//пергий if
+        }//перший if
 
 
 
